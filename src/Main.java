70b0590b29283339ba44fa1724989a45e6b1ace8
spoona96 <dan_spooner15@hotmail.co.uk>
2022-06-27 23:44:59 +0100

    WOOO it works and passes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Locale;
 
 public class Main {
 
@@ -10,41 +7,11 @@ public class Main {
         main.findWords(new String[]{"qwety", "qwety", "qad", "qwety"});
     }
 
-//    public String[] cry(String[] words) {
-//        char[] rowOne = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'};
-//        char[] rowTwo = {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'};
-//        char[] rowThree = {'z', 'x', 'c', 'v', 'b', 'n', 'm'};
-//
-//        StringBuilder resultWord = new StringBuilder();
-//        String[] resultArray = new String[0];
-//        int i;
-//        int j;
-//        int k;
-//        for (k = 0; k < words.length; k++) {
-//            String word = words[k].toLowerCase(Locale.ROOT);
-//            char[] wordArray = word.toCharArray();
-//            for (i = 0; i < rowTwo.length; i++) {
-//                for (j = 0; j < word.length(); j++) {
-//                    if (wordArray[i] == rowTwo[j]) {
-//                        resultWord.append(wordArray[i]);
-//                        System.out.println(resultWord);
-//                    }
-//                    break;
-//                }
-//            }
-//            if (!word.equals(words[k])) {
-//                break;
-//            }
-//            resultArray[k] = Arrays.toString(new String[]{String.valueOf(resultWord)});
-//        }
-//        return resultArray;
-//    }
-
     public String[] findWords(String[] words) {
 
-        String rowStringOne = "qwertyuiop";
-        String rowStringTwo = "asdfghjkl";
-        String rowStringThree = "zxcvbnm";
+        String rowStringOne = "qwertyuiopQWERTYUIOP";
+        String rowStringTwo = "asdfghjklASDFGHJKL";
+        String rowStringThree = "zxcvbnmZXCVBNM";
 
         String[] keyboard = {rowStringOne, rowStringTwo, rowStringThree};
         int i;
@@ -58,7 +25,7 @@ public class Main {
             String row = keyboard[p];
             for (k = 0; k < words.length; k++) {
                 StringBuilder result = new StringBuilder();
-                String word = words[k].toLowerCase(Locale.ROOT);
+                String word = words[k];
                 char[] wordArray = word.toCharArray();
                 for (i = 0; i < wordArray.length; i++) {
                     char myChar = wordArray[i];
@@ -74,11 +41,7 @@ public class Main {
                     j++;
                 }
             }
-
         }
-        System.out.println(Arrays.toString(resultArray.toArray(new String[0])));
         return resultArray.toArray(new String[0]);
     }
 }
-
-
